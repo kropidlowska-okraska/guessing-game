de7d159b65908254fed527e6dbe277bc3617e974
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-14 11:14:13 +0100

    Add linear-gradient to screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
-import { Stack } from 'expo-router';
+import { LinearGradient } from "expo-linear-gradient";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "#ddb52f" }
-      }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="game" />
-      <Stack.Screen name="game-over" />
-    </Stack>
-  );
+	return (
+		<LinearGradient
+        colors={['#4e0329', '#ddb52f']}
+        style={{ flex: 1 }}
+		>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+					contentStyle: { backgroundColor: "transparent" },
+				}}
+			>
+				<Stack.Screen name="index" />
+				<Stack.Screen name="game" />
+				<Stack.Screen name="game-over" />
+			</Stack>
+		</LinearGradient>
+	);
 }

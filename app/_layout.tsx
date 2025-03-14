0cef21af2c306914f8a49ffd70dd95e8b403038d
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-14 13:20:42 +0100

    Add SafeAreaView to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,22 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native";
 
 export default function RootLayout() {
 	return (
-		<LinearGradient
-        colors={['#4e0329', '#ddb52f']}
-        style={{ flex: 1 }}
-		>
-			<Stack
-				screenOptions={{
-					headerShown: false,
-					contentStyle: { backgroundColor: "transparent" },
-				}}
-			>
-				<Stack.Screen name="index" />
-				<Stack.Screen name="game" />
-				<Stack.Screen name="game-over" />
-			</Stack>
+		<LinearGradient colors={["#4e0329", "#ddb52f"]} style={{ flex: 1 }}>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+						contentStyle: { backgroundColor: "transparent" },
+					}}
+				>
+					<Stack.Screen name="index" />
+					<Stack.Screen name="game" />
+					<Stack.Screen name="game-over" />
+				</Stack>
+			</SafeAreaView>
 		</LinearGradient>
 	);
 }

e02ab387639dfbd632ba7e2763a518d58fd4ac9a
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-17 17:10:28 +0100

    Use maxWidth and Dimensions API

--- components/game/NumberContainer.tsx
+++ components/game/NumberContainer.tsx
@@ -1,34 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
-import Colors from '../../constants/colors';
+import Colors from "../../constants/colors";
 
 type Props = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 function NumberContainer({ children }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.numberText}>{children}</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.numberText}>{children}</Text>
+		</View>
+	);
 }
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get("screen").width;
+const isSmallDevice = deviceWidth < 380;
+
 const styles = StyleSheet.create({
-  container: {
-    borderWidth: 4,
-    borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  numberText: {
-    color: Colors.accent500,
-    fontSize: 36,
-    fontFamily: 'open-sans-bold',
-  },
+	container: {
+		borderWidth: 4,
+		borderColor: Colors.accent500,
+		padding: isSmallDevice ? 12 : 24,
+		margin: isSmallDevice ? 12 : 24,
+		borderRadius: 8,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	numberText: {
+		color: Colors.accent500,
+		fontSize: 36,
+		fontFamily: "open-sans-bold",
+	},
 });
